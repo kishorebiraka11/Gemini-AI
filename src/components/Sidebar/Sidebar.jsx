599f225859ab74e9f21bae059b97ab9ca6c44ee3
Kishorebiraka <kishorebiraka11@gmail.com>
2024-09-26 23:53:19 +0530

    components folder created

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Sidebar/Sidebar.css";
+
+const Sidebar = () => {
+	return (
+		<div className="sidebar">
+			<div className="top">
+        hello
+      </div>
+			<div className="bottom"></div>
+		</div>
+	);
+};
+
+export default Sidebar;
